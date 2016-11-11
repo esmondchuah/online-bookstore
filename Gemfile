3e47c67dfa6df9a3d617a03d056ea7ca6145cc95
esmondchuah <esmondchuah@gmail.com>
2016-11-11 17:42:43 +0800

    integrated CanCan and Rolify to restrict access to resources depending on roles

--- Gemfile
+++ Gemfile
@@ -26,11 +26,15 @@ gem 'jbuilder', '~> 2.5'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 # Use bootstrap for styling
-gem 'bootstrap-sass'
-# Use devise for user authentication
-gem 'devise', '~> 4.2.0'
+gem 'bootstrap-sass', '~> 3.3.7'
 # Use haml for markup, instead of erb
 gem 'haml', '~> 4.0.7'
+# Use devise for user authentication
+gem 'devise', '~> 4.2.0'
+# CanCan is an authorization library which restricts what resources a given user is allowed to access.
+gem 'cancancan', '~> 1.15.0'
+# Roles library without any authorization enforcement supporting scope on resource object.
+gem 'rolify', '~> 5.1.0'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161110143118) do
+ActiveRecord::Schema.define(version: 20161111090115) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "roles", force: :cascade do |t|
+    t.string   "name"
+    t.string   "resource_type"
+    t.integer  "resource_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.index ["name", "resource_type", "resource_id"], name: "index_roles_on_name_and_resource_type_and_resource_id", using: :btree
+    t.index ["name"], name: "index_roles_on_name", using: :btree
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "username",               default: "", null: false
@@ -34,6 +44,12 @@ ActiveRecord::Schema.define(version: 20161110143118) do
     t.datetime "updated_at",                          null: false
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
     t.index ["username"], name: "index_users_on_username", unique: true, using: :btree
+  end
+
+  create_table "users_roles", id: false, force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "role_id"
+    t.index ["user_id", "role_id"], name: "index_users_roles_on_user_id_and_role_id", using: :btree
   end
 
 end
