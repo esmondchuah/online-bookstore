a02a000ed3320906ea2f412a429bcc656b6cf6fb
esmondchuah <esmondchuah@gmail.com>
2016-11-16 23:39:54 +0800

    basic styling for users books panel

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,3 +1,10 @@
 class BooksController < ApplicationController
+  def index
+    @books = Book.all
+  end
 
+  def show
+    @book = Book.find(params[:id])
+    @cart
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users, controllers: { registrations: 'users/registrations', sessions: 'users/sessions' }
 
-  namespace :admins do
+  namespace :admin do
     resources :books
     root 'books#index'
   end
+
+  resources :books
 
   root 'books#index'
 end
