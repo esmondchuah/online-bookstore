3c49b6e40712debe896594e401f083429c973aed
Esmond Chuah <esmondchuah@gmail.com>
2016-11-30 18:32:48 +0800

    allow users to change quantity on checkout page

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -3,10 +3,17 @@ class CartsController < ApplicationController
 
   def index
     @carts = current_user.carts
+    @total_price = total_price
   end
 
   def create
     current_user.create_cart(cart_params)
+    redirect_to :back
+  end
+
+  def update
+    @cart = Cart.find(params[:id])
+    @cart.update(cart_params)
     redirect_to :back
   end
 
@@ -20,5 +27,11 @@ class CartsController < ApplicationController
 
   def cart_params
     params.require(:cart).permit(:quantity, :book_id)
+  end
+
+  def total_price
+    sum = 0
+    current_user.carts.each {|cart| sum += cart.book.price * cart.quantity}
+    sum
   end
 end
