00222124c8f561d1eeac8760589369b6af495a79
Esmond Chuah <esmondchuah@gmail.com>
2016-12-02 01:22:10 +0800

    added sorting function to book index page

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,11 @@
 class BooksController < ApplicationController
   def index
     @books = Book.filter(params.slice(:title, :authors, :publisher, :subject))
+    if params[:sort] == "year"
+      @books = @books.order(year: :desc)
+    # elsif params[:sort] == "review score"
+    #   @books = @books.order(average_score: :desc)
+    end
   end
 
   def show
